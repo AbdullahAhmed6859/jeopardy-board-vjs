84254d38c373f1328d9cb318c4a2fdeb9d988375
Abdullah Ahmed <abdullahahmed2004@gmail.com>
2025-01-25 11:39:58 +0500

    Maths fixed

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 const game = document.getElementById("game");
 
 import a6 from "./assets/images/a6.png";
-import q6 from "./assets/images/q6.png";
+import q6 from "./assets/images/q6.jpg";
 import bio_q6 from "./assets/images/bio_q6.png";
 
 const jeopardyCategories = [
@@ -23,28 +23,28 @@ const jeopardyCategories = [
       },
       {
         question:
-          "3) The intensity of radiation emitted by a black body is directly proportional to the fourth power of temperature in Kelvin. This statement is known as the<br/><br/>A. Compton's effect.<br/>B. Plank's radiation law.<br/>C. Stefan-Boltzmann law.<br/>D. De-Broglie's hypothesis.",
-        answer: "C. Stefan-Boltzmann law.",
+          "3) The intensity of radiation emitted by a black body is directly proportional to the fourth power of temperature in Kelvin. This statement is known as the<br/><br/>A) Compton's effect.<br/>B) Plank's radiation law.<br/>C) Stefan-Boltzmann law.<br/>D) De-Broglie's hypothesis.",
+        answer: "C) Stefan-Boltzmann law.",
         options: [],
         level: 30,
       },
       {
         question:
-          "4) Which of the following best describes the behavior of an electron in the double-slit experiment?<br/><br/>A. The electron always follows a single, predictable path <br/>B. The electron behaves like a classical particle with a definite trajectory <br/>C. The electron demonstrates both wave-like and particle-like properties<br/>D. The electron disappears when observed",
+          "4) Which of the following best describes the behavior of an electron in the double-slit experiment?<br/><br/>A) The electron always follows a single, predictable path <br/>B) The electron behaves like a classical particle with a definite trajectory <br/>C) The electron demonstrates both wave-like and particle-like properties<br/>D) The electron disappears when observed",
         answer:
           "The electron demonstrates both wave-like and particle-like properties",
         level: 40,
       },
       {
         question:
-          "5) A gas is compressed at a constant pressure of 50 N/m^2 from a volume of 10m^3To a volume of 4m^3. Energy of 100j is then added to the gas by heating. Its internal energy is<br/><br/>A. increased by 400j<br/>B. increased by 200j<br/>C. increased by 100j<br/>D. decreased by 200j",
-        answer: "A. increased by 400j",
+          "5) A gas is compressed at a constant pressure of 50 N/m^2 from a volume of 10m^3To a volume of 4m^3. Energy of 100j is then added to the gas by heating. Its internal energy is<br/><br/>A) increased by 400j<br/>B) increased by 200j<br/>C) increased by 100j<br/>D) decreased by 200j",
+        answer: "A) increased by 400j",
         level: 50,
       },
       {
         question:
-          "6) A projectile is launched at an angle of 45° with an initial velocity of 20 m/s. Calculate its maximum height.<br/><br/>A.5<br/>B.10<br/>C.15<br/>D.20",
-        answer: "B.10",
+          "6) A projectile is launched at an angle of 45° with an initial velocity of 20 m/s. Calculate its maximum height.<br/><br/>A)5<br/>B)10<br/>C)15<br/>D)20",
+        answer: "B)10",
         level: 60,
       },
     ],
@@ -54,33 +54,33 @@ const jeopardyCategories = [
     questions: [
       {
         question:
-          "Viruses are called non-cellular infections entities because:<br/><br/>A. They are pathogens and cause infections<br/>B. They can replicate inside and outside the living cell<br/>C. They cause infections and can only replicate inside living cell<br/>D. They can affect the host",
+          "Viruses are called non-cellular infections entities because:<br/><br/>A) They are pathogens and cause infections<br/>B) They can replicate inside and outside the living cell<br/>C) They cause infections and can only replicate inside living cell<br/>D) They can affect the host",
         answer:
-          "C. They cause infections and can only replicate inside living cell",
+          "C) They cause infections and can only replicate inside living cell",
         level: 10,
       },
       {
         question:
-          "Which of the following is not a function of the liver?<br/><br/>A. Detoxification of harmful substances<br/>B. Production of bile<br/>C. Regulation of blood sugar levels<br/>D. Storage of oxygen",
-        answer: "D. Storage of oxygen",
+          "Which of the following is not a function of the liver?<br/><br/>A) Detoxification of harmful substances<br/>B) Production of bile<br/>C) Regulation of blood sugar levels<br/>D) Storage of oxygen",
+        answer: "D) Storage of oxygen",
         level: 20,
       },
       {
         question:
-          "Which equation represents aerobic respiration in plant cells?<br/><br/>A. 6CO2 + 6H2O → C6H12O6 + 6O2<br/>B. 2C2H5OH + 2CO2 → C6H12O6<br/>C. C6H12O6 → 2C2H5OH + 2CO2<br/>D. C6H12O6 + 6O2 → 6CO2 + 6H2O",
-        answer: "D. C6H12O6 + 6O2 → 6CO2 + 6H2O",
+          "Which equation represents aerobic respiration in plant cells?<br/><br/>A) 6CO2 + 6H2O → C6H12O6 + 6O2<br/>B) 2C2H5OH + 2CO2 → C6H12O6<br/>C) C6H12O6 → 2C2H5OH + 2CO2<br/>D) C6H12O6 + 6O2 → 6CO2 + 6H2O",
+        answer: "D) C6H12O6 + 6O2 → 6CO2 + 6H2O",
         level: 30,
       },
       {
         question:
-          "Which of the following is a type of sclerenchyma cells which is found in nut shells and provides protection?<br/><br/>A. Fibre<br/>B. Vessel<br/>C. Sclereid<br/>D. Tracheid",
-        answer: "C. Sclereid",
+          "Which of the following is a type of sclerenchyma cells which is found in nut shells and provides protection?<br/><br/>A) Fibre<br/>B) Vessel<br/>C) Sclereid<br/>D) Tracheid",
+        answer: "C) Sclereid",
         level: 40,
       },
       {
         question:
-          "A compound, analogous to alcohols, with the general formula RSH and a disagreeable odour is termed as<br/><br/>A. enol.<br/>B. thiol.<br/>C. borole.<br/>D. pyrrole.",
-        answer: "B. thiol.",
+          "A compound, analogous to alcohols, with the general formula RSH and a disagreeable odour is termed as<br/><br/>A) enol.<br/>B) thiol.<br/>C) borole.<br/>D) pyrrole.",
+        answer: "B) thiol.",
         level: 50,
       },
       {
@@ -108,13 +108,13 @@ const jeopardyCategories = [
       },
       {
         question:
-          "What is the magnitude of the vector 12i-8j-24k?<br/><br/>(A) 18<br/>(B) 28 )<br/>(C) 38<br/>(D) 48",
+          "What is the magnitude of the vector 12i-8j-24k?<br/><br/>(A) 18<br/>(B) 28<br/>(C) 38<br/>(D) 48",
         answer: "B) 28",
         level: 30,
       },
       {
         question:
-          "If is one of the cube roots of unity, -20 will be<br/><br/>A) -1<br/>B) -w<br/>C) w<br/>D) 2w",
+          "If w is one of the cube roots of unity, w^(-20) will be<br/><br/>A) -1<br/>B) -w<br/>C) w<br/>D) 2w",
         answer: "C) w",
         level: 40,
       },
@@ -125,8 +125,8 @@ const jeopardyCategories = [
         level: 50,
       },
       {
-        question: `<img src=${q6} alt='Description of the image'>`,
-        answer: `<img src=${a6} alt='Description of the image'>`,
+        question: `<img src=${q6} width='800' alt='Description of the image'><br/><br/>A) 16 <br/> B) 26 <br/> C) 30 <br/> D)20`,
+        answer: "B) 26",
         level: 60,
       },
     ],
@@ -136,39 +136,39 @@ const jeopardyCategories = [
     questions: [
       {
         question:
-          "The identification of functional groups within a molecule is easily accomplished by:<br/><br/>A. mass spectrometry.<br/>B. infrared spectroscopy. <br/>C. ultraviolet-visible spectroscopy.<br/>D. nuclear magnetic resonance spectroscopy.",
-        answer: "B. infrared spectroscopy.",
+          "The identification of functional groups within a molecule is easily accomplished by:<br/><br/>A) mass spectrometry.<br/>B) infrared spectroscopy. <br/>C) ultraviolet-visible spectroscopy.<br/>D) nuclear magnetic resonance spectroscopy.",
+        answer: "B) infrared spectroscopy.",
         level: 10,
       },
       {
         question:
-          "Balance the chemical equation: C3H8 + O2 → CO2 + H2O.<br/><br/>A. C3H8 + 5 O2 → 3 CO2 + 4 H20 <br/>B. C3H8 + 4 O2 → 3 CO2 + 4 H20<br/>C. C3H8 + 6 O2 → 3 CO2 + 4 H20<br/>D. C3H8 + 3 O2 → 3 CO2 + 5 H20",
-        answer: "A. C3H8 + 5 O2 → 3 CO2 + 4 H20",
+          "Balance the chemical equation: C3H8 + O2 → CO2 + H2O.<br/><br/>A) C3H8 + 5 O2 → 3 CO2 + 4 H20 <br/>B) C3H8 + 4 O2 → 3 CO2 + 4 H20<br/>C) C3H8 + 6 O2 → 3 CO2 + 4 H20<br/>D) C3H8 + 3 O2 → 3 CO2 + 5 H20",
+        answer: "A) C3H8 + 5 O2 → 3 CO2 + 4 H20",
         level: 20,
       },
       {
         question:
-          "When a non-volatile solute is added to a pure solvent, the<br/><br/>A. boiling point of the solution decreases.<br/>B. freezing point of the solution increases.<br/>C. vapour pressure of the solvent increases.<br/>D. rate of evaporation of the solvent decreases. ",
-        answer: "D. rate of evaporation of the solvent decreases.",
+          "When a non-volatile solute is added to a pure solvent, the<br/><br/>A) boiling point of the solution decreases.<br/>B) freezing point of the solution increases.<br/>C) vapour pressure of the solvent increases.<br/>D) rate of evaporation of the solvent decreases. ",
+        answer: "D) rate of evaporation of the solvent decreases.",
         level: 30,
       },
       {
         question:
-          "How many moles of oxygen gas are needed for the complete combustion of 1mol of (CH3)3COH?<br/><br/>A. 6<br/>B. 6.5<br/>C. 12<br/>D. 13",
-        answer: "A. 6",
+          "How many moles of oxygen gas are needed for the complete combustion of 1mol of (CH3)3COH?<br/><br/>A) 6<br/>B) 6.5<br/>C) 12<br/>D) 13",
+        answer: "A) 6",
         level: 40,
       },
       {
         question:
-          "Which of the following statements is FALSE for a phenol?<br/><br/>A. The conjugate base of phenol is more stable than a phenol molecule.<br/>B. The addition of a strong base to a phenol produces a reactive nucleophile.<br/>C. The O–H group attached to a benzene ring in phenol shows acidic properties.<br/>D. The negative charge in the phenoxide ion remains localized on the oxygen atom.",
+          "Which of the following statements is FALSE for a phenol?<br/><br/>A) The conjugate base of phenol is more stable than a phenol molecule.<br/>B) The addition of a strong base to a phenol produces a reactive nucleophile.<br/>C) The O–H group attached to a benzene ring in phenol shows acidic properties.<br/>D) The negative charge in the phenoxide ion remains localized on the oxygen atom.",
         answer:
-          "D. The negative charge in the phenoxide ion remains localized on the oxygen atom.",
+          "D) The negative charge in the phenoxide ion remains localized on the oxygen atom.",
         level: 50,
       },
       {
         question:
-          "A compound, analogous to alcohols, with the general formula RSH and a disagreeable odour is termed as<br/><br/>A. enol.<br/>B. thiol. <br/>C. borole.<br/>D. pyrrole.",
-        answer: "B. thiol.",
+          "A compound, analogous to alcohols, with the general formula RSH and a disagreeable odour is termed as<br/><br/>A) enol.<br/>B) thiol. <br/>C) borole.<br/>D) pyrrole.",
+        answer: "B) thiol.",
         level: 60,
       },
     ],
@@ -178,39 +178,39 @@ const jeopardyCategories = [
     questions: [
       {
         question:
-          "Which of the following memories can a CPU directly communicate with?<br/><br/>A. Register <br/>B. CD-ROM<br/>C. DVD-ROM<br/>D. USB Flash drive",
-        answer: "A. Register",
+          "Which of the following memories can a CPU directly communicate with?<br/><br/>A) Register <br/>B) CD-ROM<br/>C) DVD-ROM<br/>D) USB Flash drive",
+        answer: "A) Register",
         level: 10,
       },
       {
         question:
-          "Which of the following is FALSE about assembly language?<br/><br/>A. It takes less memory space as compared to high-level languages.<br/>B. Debugging of programs is simplified as compared to machine code.<br/>C. Before running on a machine, an assembly language program requires a translator.<br/>D. Each assembly language instruction generates many machine language instructions. ",
+          "Which of the following is FALSE about assembly language?<br/><br/>A) It takes less memory space as compared to high-level languages.<br/>B) Debugging of programs is simplified as compared to machine code.<br/>C) Before running on a machine, an assembly language program requires a translator.<br/>D) Each assembly language instruction generates many machine language instructions. ",
         answer:
-          "D. Each assembly language instruction generates many machine language instructions.",
+          "D) Each assembly language instruction generates many machine language instructions.",
         level: 20,
       },
       {
         question:
-          "Which of the following is not a type of database?<br/><br/>A. Relational database<br/>B. Hierarchical database<br/>C. Object-oriented database<br/>D. Virtual database ",
-        answer: "D. Virtual database",
+          "Which of the following is not a type of database?<br/><br/>A) Relational database<br/>B) Hierarchical database<br/>C) Object-oriented database<br/>D) Virtual database ",
+        answer: "D) Virtual database",
         level: 30,
-      },
-      {
-        question:
-          "Which of the following encryption algorithms is asymmetric?<br/><br/>A. RSA <br/>B. AES<br/>C. DES<br/>D. Blowfish",
-        answer: "A. RSA",
-        level: 40,
-      },
-      {
-        question:
-          "In quantum computing, what is a qubit?<br/><br/>A. A binary representation of data<br/>B. A unit of classical memory<br/>C. A quantum version of the classical bit <br/>D. A quantum algorithm",
-        answer: "C. A quantum version of the classical bit",
-        level: 50,
       },
       {
         question:
           "Which of these are not low-level programming languages? <br/><br/> a) Python <br/> b) Java <br/> c) Both a and b <br/> d) none of the above",
         answer: "c) Both A and B",
+        level: 40,
+      },
+      {
+        question:
+          "In quantum computing, what is a qubit?<br/><br/>A) A binary representation of data<br/>B) A unit of classical memory<br/>C) A quantum version of the classical bit <br/>D) A quantum algorithm",
+        answer: "C) A quantum version of the classical bit",
+        level: 50,
+      },
+      {
+        question:
+          "Which of the following encryption algorithms is asymmetric?<br/><br/>A) RSA <br/>B) AES<br/>C) DES<br/>D) Blowfish",
+        answer: "A) RSA",
         level: 60,
       },
     ],
@@ -253,13 +253,13 @@ const jeopardyCategories = [
       },
       {
         question:
-          "What is the pH of a mixture of hydrochloric acid (HCl) and vinegar? <br/><br/> A. Greater than 7 <br/> B. Less than 2.5  <br/> C. Between 2.5 and 7  <br/> D. Exactly 7",
-        answer: "B. Less than 2.5 ",
+          "What is the pH of a mixture of hydrochloric acid (HCl) and vinegar? <br/><br/> A) Greater than 7 <br/> B) Less than 2.5  <br/> C) Between 2.5 and 7  <br/> D) Exactly 7",
+        answer: "B) Less than 2.5 ",
         options: [
-          "A. Greater than 7",
-          "B. Less than 2.5",
-          "C. Between 2.5 and 7",
-          "D. Exactly 7",
+          "A) Greater than 7",
+          "B) Less than 2.5",
+          "C) Between 2.5 and 7",
+          "D) Exactly 7",
         ],
         level: 40,
       },
